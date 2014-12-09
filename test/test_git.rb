2aa8652301a923e84c166238030a6b84958d1c0f
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-12-09 15:45:31 +0100

    Add tests for path traversal errors

--- test/test_git.rb
+++ test/test_git.rb
@@ -83,12 +83,24 @@ class TestGit < Test::Unit::TestCase
     assert_equal 'bar', @git.fs_read('foo')
   end
 
+  def test_fs_read_path_traversal
+    assert_raise RuntimeError do
+      @git.fs_read('../foo')
+    end
+  end
+
   def test_fs_write
     f = stub
     f.expects(:write).with('baz')
     FileUtils.expects(:mkdir_p).with(File.join(@git.git_dir, 'foo'))
     File.expects(:open).with(File.join(@git.git_dir, 'foo/bar'), 'w').yields(f)
     @git.fs_write('foo/bar', 'baz')
+  end
+
+  def test_fs_write_path_traversal
+    assert_raise RuntimeError do
+      @git.fs_read('../foo/bar')
+    end
   end
 
   def test_fs_delete
